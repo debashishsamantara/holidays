573ddfacf200eb44eda99c168ee36b3ccb29c161
debashishsamantara <debashishsamantara1@gmail.com>
2024-02-25 17:46:16 +0530

    added feature to add hotels and e2e feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,12 @@ import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-d
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
+import AddHotel from "./pages/AddHotel";
+import { useAppContext } from "./contexts/AppContext";
 
 const App = () => {
+    const { isLoggedIn } = useAppContext();
+
     return (
         <Router>
             <Routes>
@@ -19,6 +23,11 @@ const App = () => {
                 <Route path="/sign-in" element={<Layout>
                     <SignIn />
                 </Layout>}/>
+                {isLoggedIn && <>
+                    <Route path="/add-hotel" element={<Layout>
+                        <AddHotel />
+                    </Layout>}/>
+                </>}
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </Router>

4721ed03f9c5df892c297f2af5da63545e9c7b90
debashishsamantara <debashishsamantara1@gmail.com>
2024-02-29 19:19:54 +0530

    added edit hotel feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
+import EditHotel from "./pages/EditHotel";
 
 const App = () => {
     const { isLoggedIn } = useAppContext();
@@ -30,6 +31,9 @@ const App = () => {
                     </Layout>}/>
                     <Route path="/my-hotels" element={<Layout>
                         <MyHotels />
+                    </Layout>}/>
+                    <Route path="/edit-hotel/:hotelId" element={<Layout>
+                        <EditHotel />
                     </Layout>}/>
                 </>}
                 <Route path="*" element={<Navigate to="/" />} />

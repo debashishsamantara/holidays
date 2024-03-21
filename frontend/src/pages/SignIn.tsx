69f7617e5e4c2f05e74978f8b68ee4e953278530
debashishsamantara <debashishsamantara1@gmail.com>
2024-03-21 23:51:52 +0530

    added hotel detail page

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export type SignInFormData = {
     email: string;
@@ -13,6 +13,7 @@ const SignIn = () => {
     const { showToast } = useAppContext();
     const navigate = useNavigate();
     const queryClient = useQueryClient();
+    const location = useLocation();
     const { register, formState: { errors }, handleSubmit } = useForm<SignInFormData>();
 
     const mutation = useMutation(apiClient.signIn, {
@@ -22,7 +23,7 @@ const SignIn = () => {
                 type: "SUCCESS"
             })
             await queryClient.invalidateQueries("validateToken")
-            navigate("/");
+            navigate(location.state?.from?.pathname || "/");
         },
         onError: async (error: Error) => {
             showToast({
